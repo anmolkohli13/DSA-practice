dbf178115160d626a98975885aed7caeea97cbb4
anmol kohli <51615200+anmolkohli13@users.noreply.github.com>
2021-07-10 20:45:30 +0530

    Create wordbreak.cpp

---
+++ leetcode solutions/wordbreak.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& dict) {
+     
+        if(dict.size()==0) return false;
+        
+        vector<bool> dp(s.size()+1,false);
+        dp[0]=true;
+        
+        for(int i=1;i<=s.size();i++)
+        {
+            for(int j=i-1;j>=0;j--)
+            {
+                if(dp[j])
+                {
+                    string word = s.substr(j,i-j);
+                    if(std::find(dict.begin(),dict.end(),word)!= dict.end())
+                    {
+                        dp[i]=true;
+                        break; //next i
+                    }
+                }
+            }
+        }
+        
+        return dp[s.size()];
+        
+        
+    }
+};
