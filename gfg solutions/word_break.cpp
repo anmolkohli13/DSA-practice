459ea559028838f67f30341973ee89274f343ac2
anmol kohli <51615200+anmolkohli13@users.noreply.github.com>
2021-07-10 20:50:49 +0530

    Create word_break.cpp

---
+++ gfg solutions/word_break.cpp
@@ -0,0 +1,28 @@
+int wordBreak(string s, vector<string> &dict) {
+    //code here
+    
+    if(dict.size()==0) return false;
+        
+        vector<bool> dp(s.size()+1,false);
+        dp[0]=true;
+        
+        for(int i=1;i<=s.size();i++)
+        {
+            for(int j=i-1;j>=0;j--)
+            {
+                if(dp[j])
+                {
+                    string word = s.substr(j,i-j);
+                    if(std::find(dict.begin(),dict.end(),word)!= dict.end())
+                    {
+                        dp[i]=true;
+                        break; //next i
+                    }
+                }
+            }
+        }
+        
+        return dp[s.size()];
+     
+    
+}
