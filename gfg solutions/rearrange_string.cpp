71051cb3b482d3832dd93a1138dcba590c449f84
anmol kohli <51615200+anmolkohli13@users.noreply.github.com>
2021-07-17 18:17:44 +0530

    see this as this a good question has implementation of maxheap

---
+++ gfg solutions/rearrange_string.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+    public:
+    string rearrangeString(string str)
+    {
+        //code here
+        
+        //this uses the same approach as kevin naughton video just this is for c++
+        // see this as this a good question and also has implementation of maxheap using priority queue
+        
+        priority_queue<pair<int,char>> pq;
+        unordered_map<char , int> mp;
+        
+        for(auto c:str){
+            mp[c]++;
+            if(mp[c] > (str.length()+1)/2){
+                return "-1";
+            }
+        }
+        
+        for(auto x:mp){
+            pq.push(make_pair(x.second,x.first));
+        }
+        
+        string res;
+        auto temp = pq.top();
+        pq.pop();
+        res = temp.second;
+        temp.first -=1;
+        
+        while(!pq.empty()){
+            auto top = pq.top();
+            pq.pop();
+            res +=top.second;
+            
+            if(temp.first>0){
+                pq.push({temp.first,temp.second});
+            }
+            temp = {top.first-1,top.second};
+        }
+        return res;
+        
+    }
+};
