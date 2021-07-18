b822ed72ebe084f2e7a2fb92e5446e097e7154ce
anmol kohli <51615200+anmolkohli13@users.noreply.github.com>
2021-07-18 20:11:43 +0530

    good question just use 2 hashmaps
    good question just remember to use 2 hashmaps for bidirectional checking

---
+++ leetcode solutions/isomorphic_strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isIsomorphic(string str1, string str2) {
+        
+        if(str1.length() != str2.length())
+        return false;
+        
+        int hash1[256] = {0},hash2[256]={0};
+        int i;
+        
+        for(i = 0; i < str1.length(); i++)
+        {
+            if(hash1[str1[i]] == 0 && hash2[str2[i]] == 0)
+            {
+                hash1[str1[i]] = (int)str2[i];
+                hash2[str2[i]] = (int)str1[i];
+            }
+            else
+            {
+                if(hash1[str1[i]] != (int)str2[i] || hash2[str2[i]] != (int)str1[i])
+                return false;
+            }
+        }
+        return true;
+        
+        
+    }
+};
