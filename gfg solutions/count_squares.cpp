16efea47e82c03bed1cc782ba247d7aa13629e20
anmol kohli <51615200+anmolkohli13@users.noreply.github.com>
2021-07-19 20:18:31 +0530

    look for a unique approach

---
+++ gfg solutions/count_squares.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int countSquares(int N) {
+        // code here
+        
+        int count =0;
+
+        for(int i=1; i*i < N; i++)
+        {
+        count++;
+        }
+
+        return count;
+        
+    }
+};
