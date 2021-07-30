1ad44b676c351859920ef7f0068586f1e5b26a8b
anmol kohli <51615200+anmolkohli13@users.noreply.github.com>
2021-07-31 01:28:47 +0530

    applied the logic but couldn't execute
    applied the logic but couldn't execute so watched TUF video and applied the code

---
+++ leetcode solutions/merge_intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+     
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        
+        vector<int>temp = intervals[0];
+        
+        for(auto it : intervals)
+        {
+          if(it[0] <= temp[1]) {
+                temp[1] = max(it[1], temp[1]); 
+            } else {
+                ans.push_back(temp); 
+                temp = it; 
+            }
+        }
+        ans.push_back(temp);
+        return ans;
+        
+    }
+};
