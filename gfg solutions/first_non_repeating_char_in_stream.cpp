4be0291fc84832d6bcc1d03341c1c91fd8e3bf15
anmol kohli <51615200+anmolkohli13@users.noreply.github.com>
2021-08-13 20:27:01 +0530

    good question read the statement properly

---
+++ gfg solutions/first_non_repeating_char_in_stream.cpp
@@ -0,0 +1,31 @@
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    // Code here
+		    
+		    string s = "";
+            queue<char>q;
+            int arr[26] = {0};
+            for (int i = 0; i < A.size(); i++) {
+            
+            q.push(A[i]);
+            arr[A[i] - 'a']++;
+            while (!q.empty()) {
+            if (arr[q.front() - 'a'] > 1) {
+            q.pop();
+            }
+            else
+            break;
+            }
+            if (q.empty())
+            s += '#';
+            else
+            s += q.front();
+            }
+            
+            return s;
+		    
+		    
+		}
+
+};
