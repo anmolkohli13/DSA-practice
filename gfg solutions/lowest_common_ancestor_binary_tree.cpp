5e790516ccfeff97a6c881b1954ded12d4ddb0b9
anmol kohli <51615200+anmolkohli13@users.noreply.github.com>
2021-08-28 00:01:27 +0530

    good question revise it

---
+++ gfg solutions/lowest_common_ancestor_binary_tree.cpp
@@ -0,0 +1,23 @@
+Node* lca(Node* root ,int n1 ,int n2 )
+    {
+       //Your code here 
+        Node *lnode, *rnode;
+        if(!root){
+        return NULL;
+        }
+        
+        if(root->data == n1 || root->data == n2){
+        return root;
+        }
+        
+        lnode = lca(root->left, n1, n2);
+        rnode = lca(root->right, n1, n2);
+        if(lnode && rnode){
+        return root;
+        }
+        
+        return (lnode != NULL) ? lnode : rnode;
+
+
+       
+    }
