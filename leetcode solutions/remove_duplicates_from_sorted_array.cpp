5b554d916f0041bfa596cdcc2a4f23b6db6f5438
anmol kohli <51615200+anmolkohli13@users.noreply.github.com>
2021-09-01 20:42:03 +0530

    easy question made difficult and confusing

---
+++ leetcode solutions/remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& A) {
+        
+        int count = 0;
+        int n = A.size();
+        for(int i = 1; i < n; i++){
+            if(A[i] == A[i-1]) count++;
+            else A[i-count] = A[i];
+        }
+        return n-count;
+        
+        
+    }
+};
