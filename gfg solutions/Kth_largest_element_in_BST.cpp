9d594fc3f800d6f8d2e658df2769c6b761acb97b
anmol kohli <51615200+anmolkohli13@users.noreply.github.com>
2021-09-05 17:52:24 +0530

    easy knew how to solve but TC not passing so copy

---
+++ gfg solutions/Kth_largest_element_in_BST.cpp
@@ -0,0 +1,45 @@
+/*The Node structure is defined as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+// return the Kth largest element in the given BST rooted at 'root'
+class Solution
+{
+    public:
+    
+    void revInOrder(Node *root, int k, int &index, int &res)
+    {
+        if(root != NULL)
+        {
+            revInOrder(root->right, k, index, res);
+            
+            if(index == k)
+            {
+                res = root->data;
+            }
+            index++;
+            
+            revInOrder(root->left, k, index, res);
+        }
+    }
+
+
+    int kthLargest(Node *root, int K)
+    {
+        int index = 1;
+        int res = -1;
+        
+        revInOrder(root, K, index, res);
+        
+        return res;
+    }
+};
