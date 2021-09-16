11c918177abb89f6d5034fcd8053093fed28c40c
anmol kohli <51615200+anmolkohli13@users.noreply.github.com>
2021-09-16 18:41:18 +0530

    clever solution so REVISE
    the left and the right int are the numbers of the element from the start not the values in nodes Still this is a clever solution so REVISE

---
+++ leetcode solutions/reverse_LL_II.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// the left and the right int are the numbers of the element from the start not the values in nodes 
+// Still this is a clever solution so REVISE
+
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+       ListNode *dummy = new ListNode(0), *pre = dummy, *cur;
+       dummy -> next = head;
+       for (int i = 0; i < m - 1; i++) {
+           pre = pre -> next;
+       }
+       cur = pre -> next;
+       for (int i = 0; i < n - m; i++) {
+           ListNode* temp = pre -> next;
+           pre -> next = cur -> next;
+           cur -> next = cur -> next -> next;
+           pre -> next -> next = temp;
+       }
+       return dummy -> next;
+   }
+};
