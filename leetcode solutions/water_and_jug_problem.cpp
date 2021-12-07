6a9bf060a8fea6bc26475b0b72e455462211b90d
anmol kohli <51615200+anmolkohli13@users.noreply.github.com>
2021-12-07 19:35:02 +0530

    REVISE can't come up without having this in mind

---
+++ leetcode solutions/water_and_jug_problem.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    int GCD(int a, int b)
+    {
+        while(b!= 0 )
+        {
+            int temp = b;
+            b = a%b;
+            a = temp;
+        }
+        return a;
+    }
+    
+    bool canMeasureWater(int x, int y, int z) {
+        
+        if(x + y < z)
+            return false;
+        if(x == z || y == z || x + y == z)
+            return true;
+        
+        return z%GCD(x,y) == 0;
+    }
+};
