09c45e687596632951b52af39e240252ada2a57a
anmol kohli <51615200+anmolkohli13@users.noreply.github.com>
2022-01-25 19:15:03 +0530

    Create maximize_cuts.cpp

---
+++ gfg solutions/maximize_cuts.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:
+    //Function to find the maximum number of cuts.
+    int maximizeTheCuts(int n, int x, int y, int z)
+    {
+        int a[]={x,y,z};
+        int dp[3+1][n+1];
+        for(int i=0;i<=3;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                if(i==0 || j==0)
+                {
+                    if(i==0)
+                        dp[i][j]=INT_MIN;
+                    else if(j==0) 
+                        dp[i][j]=0;
+                
+                }
+                else if(a[i-1]<=j)
+                {
+                    dp[i][j]=max(1+dp[i][j-a[i-1]],dp[i-1][j]);
+                }
+                else 
+                    dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[3][n]<0?0:dp[3][n];
+                
+        
+    
+
+    }
+};
